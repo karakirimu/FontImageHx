c8fd85086539d3fbb8a321d64e80e5fb00419ac7
karakirimu <37269014+karakirimu@users.noreply.github.com>
2023-05-18 23:59:27 +0900

    [Update] Add about window

--- FontBmpGen/About.xaml.cs
+++ FontBmpGen/About.xaml.cs
@@ -1,19 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Navigation;
 
 namespace FontBmpGen
 {
+    public class AboutWindowViewModel : INotifyPropertyChanged
+    {
+        public AboutWindowViewModel() 
+        {
+            System.Reflection.Assembly assembly
+                = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo fvi
+                = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+            _softwareVersion = $"FontBitmapGenerator v{fvi.ProductVersion}";
+        }
+
+        private string _softwareVersion;
+        public string SoftwareVersion
+        {
+            get => _softwareVersion;
+            set => SetProperty(ref _softwareVersion, value);
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+    }
+
     /// <summary>
     /// About.xaml の相互作用ロジック
     /// </summary>
@@ -22,11 +48,20 @@ namespace FontBmpGen
         public AboutWindow()
         {
             InitializeComponent();
+
+            var viewModel = new AboutWindowViewModel();
+            DataContext = viewModel;
         }
 
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start("explorer", e.Uri.AbsoluteUri);
+            e.Handled = true;
         }
     }
 }
